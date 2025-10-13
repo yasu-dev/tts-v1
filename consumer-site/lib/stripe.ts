f0bde5a36e11b2b461be2d280b696b4cf3c859f3
leadx <tbnkin@gmail.com>
2025-10-13 17:54:25 +0900

    fix: update Stripe API version to 2025-09-30.clover

--- consumer-site/lib/stripe.ts
+++ consumer-site/lib/stripe.ts
@@ -6,6 +6,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-09-30.clover',
   typescript: true,
 });
